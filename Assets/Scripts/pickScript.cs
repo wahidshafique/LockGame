ba1037e0ffe800660a7bfad593ce67c9adf9d253
Wahid Shafique <wahidshafique@hotmail.com>
2017-02-12 21:50:05 -0500

    add base files

---
+++ Assets/Scripts/pickScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickScript : MonoBehaviour {
+
+    void Start() {
+
+    }
+
+    void Update() {
+        rotatePick();
+    }
+
+    void rotatePick() {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.x -= Screen.width / 2;
+        mousePos += transform.position * 2;
+        float angle = Vector3.Angle(mousePos, Vector3.up);
+        if (mousePos.x > 0) {
+            angle = 360 - angle;
+        }
+        Debug.Log(this.transform.rotation.eulerAngles);
+        this.transform.rotation = Quaternion.Euler(angle, 0, 0);
+    }
+}
