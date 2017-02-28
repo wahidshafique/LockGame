bd4d7d9c6d335322907db74338fd4967f9edf941
Wahid Shafique <wahidshafique@hotmail.com>
2017-02-27 23:11:52 -0500

    add some rotation'

---
+++ Assets/EventHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandler : MonoBehaviour {
+    public GameObject tensionWrench;
+    private Vector3 tensionPos;
+    public pickScript hitPick;
+    public GlowObject glowPick;
+    // Use this for initialization
+    void Start() {
+        tensionPos = tensionWrench.transform.position;
+    }
+    // Update is called once per frame
+    void Update() {
+        ClickPick();
+        SnapTension();
+    }
+
+    void ClickPick() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100)) {
+                if (hit.collider.tag == "Pick") {
+                    print("hitting pick");
+                    hitPick.isBeingHit = !hitPick.isBeingHit;
+                    if (hitPick.isBeingHit) {
+                        glowPick.ColorEnter();
+                    } else {
+                        glowPick.ColorLeave();
+                    }
+                }
+            }
+        }
+    }
+
+    void SnapTension() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (hitPick.isBeingHit) {
+                Vector3 newPos = new Vector3(tensionPos.x, tensionPos.y - 0.01f, tensionPos.z);
+                Vector3 vel = Vector3.zero;
+                tensionWrench.transform.position = Vector3.SmoothDamp(transform.position, newPos, ref vel, 0.1f);
+            }
+
+        }
+    }
+}
--- Assets/Scripts/pickScript.cs
+++ Assets/Scripts/pickScript.cs
@@ -1,26 +1,72 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class pickScript : MonoBehaviour {
+    public bool isBeingHit = false;
+    //this pick object is childed to this
+    private GameObject pickObject;
+    //this is the rotund end of the pick, it glows (for debug?)
+    private GlowObject glowPick;
+    //the parent game object can only rotate to these angles on X
+    private float angleThresh = 90.0f;
+    //the final mouse pos
+    Vector3 v;
+    int[] range;
 
     void Start() {
-
+        glowPick = GetComponentInChildren<GlowObject>();
+        range = SweetSpotRange(0, 5);
+        //this is just the pick (for changing colors, etc)
+        //needs to be seperate so it has different anchor
+        pickObject = transform.GetChild(0).gameObject;
     }
 
     void Update() {
-        rotatePick();
+        if (isBeingHit) {
+            RotatePick();
+        }
     }
 
-    void rotatePick() {
+    void RotatePick() {
         Vector3 mousePos = Input.mousePosition;
+        //reduce the mouses positional range
         mousePos.x -= Screen.width / 2;
-        mousePos += transform.position * 2;
-        float angle = Vector3.Angle(mousePos, Vector3.up);
-        if (mousePos.x > 0) {
-            angle = 360 - angle;
+        mousePos.y -= Screen.height / 2;
+        v = mousePos + v / 2;
+
+        float angleRads = Mathf.Atan2(v.x, v.y);
+        float angleDegs = angleRads * Mathf.Rad2Deg;
+        if (angleDegs > angleThresh) {
+            angleDegs = angleThresh;
+        } else if (angleDegs < -angleThresh) {
+            angleDegs = -angleThresh;
         }
-        Debug.Log(this.transform.rotation.eulerAngles);
-        this.transform.rotation = Quaternion.Euler(angle, 0, 0);
+        this.transform.rotation = Quaternion.Euler(angleDegs, 0, 0);
+        int floored = (int)Mathf.Floor(angleDegs);
+
+
+
+        foreach (int x in range) {
+            print(x);
+            if (x.Equals(floored)) {
+                glowPick.ColorSuccess();
+                Debug.Log("Find the int ..." + x);
+                pickObject.GetComponent<Renderer>().material.color = Color.red;
+                return;
+            } else {
+                pickObject.GetComponent<Renderer>().material.color = Color.blue;
+                glowPick.ColorEnter();
+            }
+        }
+    }
+
+    int[] SweetSpotRange(int sweetSpot, int steps) {
+        int[] sweetSpotRanger = new int[steps];
+        sweetSpotRanger[0] = sweetSpot;
+        for (int i = 1; i < sweetSpotRanger.Length; i++) {
+            sweetSpotRanger[i] = sweetSpotRanger[i - 1] + 1;
+            print(sweetSpotRanger[i]);
+        }
+        return sweetSpotRanger;
     }
 }
